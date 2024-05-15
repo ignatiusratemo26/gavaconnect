42fcc518db5db16ec1d8711d55f05e9e819489ee
ignatiusratemo26 <ignatiusratemo5@gmail.com>
2024-05-15 10:42:13 +0300

    Model updates

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Project, Comment, ProgressUpdate, BursaryApplication, GovernmentFeedback, Analytics
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Project)
+admin.site.register(Comment)
+admin.site.register(ProgressUpdate)
+admin.site.register(BursaryApplication)
+admin.site.register(GovernmentFeedback)
+admin.site.register(Analytics)
--- gavaconnect/urls.py
+++ gavaconnect/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base_app.urls')),  # Assuming your app is named 'base_app'
+    path('', include('base.urls')),
 ]
